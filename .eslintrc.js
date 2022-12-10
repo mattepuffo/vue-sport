3bbcc47a25c0c4946b4a54a812061d1c313ee43a
mattepuffo <matteo.ferrone@hotmail.it>
2022-12-10 13:56:34 +0100

    0.0.7 - aggiornamento librerie

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
 	},
 	extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
 	parserOptions: {
-		parser: 'babel-eslint',
+		// parser: 'babel-eslint',
 	},
 	rules: {
 		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
